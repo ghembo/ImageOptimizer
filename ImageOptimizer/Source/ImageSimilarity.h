8e28d71aea3b21ed0e00d8fba67ecbc92679eca8
ghembo <invictus86@gmail.com>
2017-03-13 10:00:52 +0100

    Remove unused function declaration from ImageSimilarity

--- ImageOptimizer/Source/ImageSimilarity.h
+++ ImageOptimizer/Source/ImageSimilarity.h
@@ -5,12 +5,5 @@ namespace cv
 
 namespace ImageSimilarity
 {
-	struct _kernel {
-		int w;                  /**< The kernel width */
-		int h;                  /**< The kernel height */
-	};
-
 	float ComputeSsim(const cv::Mat& referenceImage, const cv::Mat& compareImage);
-	void decimate(float *img, int width, int height, int factor, float *result, int *rw, int *rh);
-	void convolve(float *img, int width, int height, const _kernel *k, float *result, int *rw, int *rh);
 }
