664566d82acf4170294e8294a9191e691cf2c8d5
ghembo <invictus86@gmail.com>
2017-03-14 11:58:46 +0100

    ConsoleApp: process image or folder depending on command line input

--- ConsoleApp/ConsoleApp.cpp
+++ ConsoleApp/ConsoleApp.cpp
@@ -3,11 +3,14 @@
 #include "ImageOptimizer.h"
 
 #include <boost/program_options.hpp>
-namespace po = boost::program_options;
+#include <boost/filesystem.hpp>
 
 #include <iostream>
 #include <sstream>
 #include <iomanip>
+
+namespace po = boost::program_options;
+namespace fs = boost::filesystem;
 
 
 
@@ -27,6 +30,13 @@ int main(int argc, char* argv[])
 	po::store(po::command_line_parser(argc, argv).options(desc).positional(p).run(), vm);
 	po::notify(vm);
 
+	if (!fs::exists(input))
+	{
+		std::cout << "Input file or folder doesn't exist" << std::endl;
+
+		return 1;
+	}
+
 	std::cout << ImageOptimizer::GetVersion() << std::endl;
 
 	ImageOptimizer::EnableFileLogging();
@@ -35,7 +45,20 @@ int main(int argc, char* argv[])
 
 	try
 	{
-		imageOptimizer.OptimizeFolderRecursive(input, 0.9999f);
+		if (fs::is_regular_file(input))
+		{
+			imageOptimizer.OptimizeImage(input, 0.9999f);
+		}
+		else if (fs::is_directory(input))
+		{
+			imageOptimizer.OptimizeFolderRecursive(input, 0.9999f);
+		}
+		else
+		{
+			std::cout << "Input isn't a regular file or directory" << std::endl;
+
+			return 1;
+		}
 	}
 	catch (const std::exception& e)
 	{
